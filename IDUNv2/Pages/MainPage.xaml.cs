7a831419fafab71f139e0f1a87a572382d3c7898
Mårten Hansson <hmrten@gmail.com>
2016-05-16 10:14:31 +0200

    setup basic structure for views and models

---
+++ IDUNv2/Pages/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using IDUNv2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IDUNv2.Pages
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel viewModel = new MainViewModel();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = viewModel;
+        }
+
+        private void MainMenu_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as AppBarButton;
+            viewModel.SubMenuList = viewModel.subMenus[0];
+            ContentFrame.Navigate(viewModel.SubMenuList.FirstOrDefault().PageType);
+        }
+
+        private void SubMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var lb = sender as ListBox;
+            var item = lb.SelectedItem as SubMenuItem;
+            if (item != null)
+                ContentFrame.Navigate(item.PageType);
+        }
+
+        private void MainMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var lv = sender as ListView;
+            var item = lv.SelectedItem as MainMenuItem;
+            viewModel.SubMenuList = viewModel.subMenus[lv.SelectedIndex];
+            ContentFrame.Navigate(item.PageType);
+        }
+    }
+}
---
+++ IDUNv2/ViewModels/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDUNv2.ViewModels
+{
+    public class SubMenuItem
+    {
+        public string Title { get; set; }
+        public Type PageType { get; set; }
+    }
+
+    public class MainMenuItem
+    {
+        public string Label { get; set; }
+        public string Icon { get; set; }
+        public Type PageType { get; set; }
+    }
+
+    public class MainViewModel : BaseViewModel
+    {
+        public List<List<SubMenuItem>> subMenus = new List<List<SubMenuItem>>()
+            {
+                new List<SubMenuItem>()
+                {
+                    new SubMenuItem { Title = "Index", PageType = typeof(Pages.Home.IndexPage) },
+                    new SubMenuItem { Title = "Home 2", PageType = typeof(Pages.Home.Home2Page) },
+                    new SubMenuItem { Title = "Home 3" }
+                },
+                new List<SubMenuItem>()
+                {
+                    new SubMenuItem { Title = "Index", PageType = typeof(Pages.Settings.IndexPage) },
+                    new SubMenuItem { Title = "Pressure" },
+                    new SubMenuItem { Title = "Humidity" }
+                },
+
+        };
+
+        private List<SubMenuItem> _subMenulist;
+        public List<SubMenuItem> SubMenuList { get { return _subMenulist; } set { _subMenulist = value; Notify(); } }
+
+
+        private List<MainMenuItem> mainMenu = new List<MainMenuItem>()
+        {
+            new MainMenuItem { Label = "Home", Icon = "Home", PageType=typeof(Pages.Home.IndexPage) },
+            new MainMenuItem { Label = "Settings", Icon = "Setting", PageType=typeof(Pages.Settings.IndexPage) }
+        };
+
+        public List<MainMenuItem> MainMenuList { get { return mainMenu; } set { mainMenu = value; } }
+    }
+}
