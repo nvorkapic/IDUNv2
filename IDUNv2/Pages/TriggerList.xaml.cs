fd1913161d9a8875c3b2da1b1a861a9cbd35778b
nvorkapic <nikola.vorkapic@hotmail.com>
2016-05-31 13:08:26 +0200

    trigger list extended information

--- IDUNv2/Pages/TriggerList.xaml.cs
+++ IDUNv2/Pages/TriggerList.xaml.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -49,15 +50,13 @@ namespace IDUNv2.Pages
             {
                 
                 string WarningHeader = "Report Trigger Removed from the List";
-                string WarningContent = "Report Trigger: " + CurrentTrigger.SensorId.ToString() + " that fires when value goes" + CurrentTrigger.Comparer.ToString() + " " + CurrentTrigger.Value.ToString() + " and uses " + CurrentTrigger.TemplateId.ToString() + " TemplateId has been removed.";
+                string WarningContent = "Report Trigger: " + CurrentTrigger.SensorId.ToString() + " that fires when value goes " + CurrentTrigger.Comparer.ToString() + " " + CurrentTrigger.Value.ToString() + " and uses TemplateId " + CurrentTrigger.TemplateId.ToString() + ", has been Removed.";
                 await ss.DeleteTrigger(CurrentTrigger);
                 TriggerListView.ItemsSource = viewModel.SensorTriggerList;
-
-                var dialog = new ContentDialog { Title = WarningHeader, Content = WarningContent, HorizontalAlignment=HorizontalAlignment.Center, VerticalAlignment=VerticalAlignment.Center, PrimaryButtonText="OK"};
-   
-                var showdialog = await dialog.ShowAsync();
-
                 
+                var dialog = new ContentDialog { Title = WarningHeader,Content= WarningContent, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center, Background = new SolidColorBrush(Colors.Gainsboro), Foreground = new SolidColorBrush(Colors.Black), PrimaryButtonText="OK", SecondaryButtonText="Cancel" };
+               
+                var showdialog = await dialog.ShowAsync();  
             }
         }
 
@@ -67,6 +66,21 @@ namespace IDUNv2.Pages
             CurrentTrigger = list.SelectedItem as SensorTrigger;
         }
 
+        private async void DoubleTapList(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            var ListItem = ((ListView)sender).SelectedItem as SensorTrigger;
+            var TemplateID = (((ListView)sender).SelectedItem as SensorTrigger).TemplateId;
+            ReportService RS = new ReportService();
+            var Templates = RS.GetTemplates().Result;
+            var SelectedTemplate = Templates.Where(x => x.Id == TemplateID).FirstOrDefault();
+
+
+            var contentString ="Sensor: "+ListItem.SensorId + " " + ListItem.Comparer + " " + ListItem.Value + "\nTemplate\n Name: " + SelectedTemplate.Name + "\n Symptom: "+ SelectedTemplate.SymptCode+"\n Priority: "+ SelectedTemplate.PrioCode + "\n Discovery" + SelectedTemplate.DiscCode+ "";
+            var dialog = new ContentDialog { Title = "Selected Trigger", Content = contentString, PrimaryButtonText = "OK" };
+
+            var showdialog = await dialog.ShowAsync();
+
+        }
     }
 
 }
