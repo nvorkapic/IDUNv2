ad17511c83ac6e4398885bb49d478df76c1b8e55
nvorkapic <nikola.vorkapic@hotmail.com>
2016-06-09 13:20:04 +0200

    Triggers Binding fix, selectedValue and ValuePath for the combobox

--- IDUNv2/Pages/Sensors/SensorSettingsPage.xaml.cs
+++ IDUNv2/Pages/Sensors/SensorSettingsPage.xaml.cs
@@ -31,6 +31,7 @@ namespace IDUNv2.Pages
             set
             {
                 _comparer = value;
+                Model.Comparer = value;
                 Notify("ComparerBelow");
                 Notify("ComparerAbove");
             }
@@ -39,7 +40,7 @@ namespace IDUNv2.Pages
         public bool ComparerBelow
         {
             get { return Comparer == SensorTriggerComparer.Below; }
-            set { Comparer = SensorTriggerComparer.Below; }
+            set { Comparer = SensorTriggerComparer.Below;}
         }
 
         public bool ComparerAbove
@@ -57,9 +58,25 @@ namespace IDUNv2.Pages
             Comparer = model.Comparer;
             
         }
+        public int Id
+        {
+            get { return Model.Id; }
+            set { Model.Id = value; Notify(); }
+        }
+        public int TemplateId
+        {
+            get { return Model.TemplateId; }
+            set { Model.TemplateId = value; Notify(); }
+        }
+        public float Value
+        {
+            get { return Model.Value; }
+            set { Model.Value = value; Notify(); }
+        }
 
         public override string ToString()
         {
+  
             return $"TriggerId: {Model.Id} ON value {Model.Comparer.ToString().ToUpper()} {Model.Value} with TemplateID {Model.TemplateId}";
         }
     }
@@ -95,16 +112,18 @@ namespace IDUNv2.Pages
         public ActionCommand<object> SaveCommand { get; set; }
         public ActionCommand<object> CreateTriggerCommand { get; set; }
         public ActionCommand<object> SaveChangesTriggerCommand { get; set; }
-
+        public ActionCommand<object> RemoveTriggerCommand { get; set; }
         #endregion
 
-        public ObservableCollection<SensorTriggerViewModel> Triggers { get; set; }
+        private ObservableCollection<SensorTriggerViewModel> _triggers;
+        public ObservableCollection<SensorTriggerViewModel> Triggers { get {return _triggers; } set {_triggers = value; Notify(); } }
 
         public SensorSettingsViewModel()
         {
             SaveCommand = new ActionCommand<object>(SaveCommand_Execute);
             CreateTriggerCommand = new ActionCommand<object>(CreateTriggerCommand_Execute);
             SaveChangesTriggerCommand = new ActionCommand<object>(SaveChangesTriggerCommand_Execute);
+            RemoveTriggerCommand = new ActionCommand<object>(RemoveTriggerCommand_Execute);
             SelectedTrigger = new SensorTriggerViewModel(new SensorTrigger());
             Templates = new List<FaultReportTemplate>();
             var triggers = DAL.GetSensorTriggers().Result;
@@ -119,31 +138,50 @@ namespace IDUNv2.Pages
 
         private async void CreateTriggerCommand_Execute(object param)
         {
-           
-            var Filling = new SensorTrigger { Comparer = SelectedTrigger.Comparer, Value = SelectedTrigger.Model.Value, TemplateId = SelectedTemplate.Id };
-            if (await CheckIfTriggerExists(Filling) == false)
+           try
             {
-                Filling = await DAL.SetSensorTrigger(Filling);
-                Triggers.Add(new SensorTriggerViewModel(Filling));
+                SelectedTrigger = new SensorTriggerViewModel( new SensorTrigger {});
+                //if (await CheckIfTriggerExists(SelectedTrigger.Model) == false)
+                //{
+                SelectedTrigger.Model = await DAL.SetSensorTrigger(SelectedTrigger.Model);
+                Triggers.Add(new SensorTriggerViewModel(SelectedTrigger.Model));
+                //}
             }
+            catch
+            {
+
+            }
+            
             
         }
         private async void SaveChangesTriggerCommand_Execute(object param)
         {
-            if (SelectedTrigger != null)
                 SelectedTrigger.Model = await DAL.SetSensorTrigger(SelectedTrigger.Model);
         }
 
-        public async Task<bool> CheckIfTriggerExists(SensorTrigger Trigger)
+        private async void RemoveTriggerCommand_Execute(object param)
         {
-            var SensorTriggerList = await DAL.GetSensorTriggers();
-            foreach (var item in SensorTriggerList)
+            try
             {
-                if (item.Comparer == Trigger.Comparer && item.Value == Trigger.Value && item.TemplateId == Trigger.TemplateId)
-                    return true;
+                SelectedTrigger.Model = await DAL.DeleteSensorTrigger(SelectedTrigger.Model);
+                Triggers.Remove(SelectedTrigger);
             }
-            return false;
+            catch
+            {
+
+            }
         }
+
+        //public async Task<bool> CheckIfTriggerExists(SensorTrigger Trigger)
+        //{
+        //        var SensorTriggerList = await DAL.GetSensorTriggers();
+        //        foreach (var item in SensorTriggerList)
+        //        {
+        //            if (item.Comparer == Trigger.Comparer && item.Value == Trigger.Value && item.TemplateId == Trigger.TemplateId)
+        //                return true;
+        //        }
+        //        return false;
+        //}
     }
 
     public sealed partial class SensorSettingsPage : Page
@@ -160,6 +198,7 @@ namespace IDUNv2.Pages
 
         private async void SensorSettingsPage_Loaded(object sender, RoutedEventArgs e)
         {
+            
             viewModel.Templates = await DAL.GetFaultReportTemplates();
         }
 
@@ -182,11 +221,19 @@ namespace IDUNv2.Pages
             osk.Visibility = Visibility.Collapsed;
         }
 
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var cb = (ComboBox)sender;
+            var selectedItem = (FaultReportTemplate)cb.SelectedItem;
+            viewModel.SelectedTrigger.TemplateId = selectedItem.Id;
+        }
+
+
         //private void TemplateSelectionChange(object sender, SelectionChangedEventArgs e)
         //{
         //    var cb = (ComboBox)sender;
         //    selectedItem = (ViewModels.FaultReportTemplateViewModel)cb.SelectedItem;
-            
+
         //}
 
     }
