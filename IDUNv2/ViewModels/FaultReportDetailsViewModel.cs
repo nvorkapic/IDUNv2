77e91789e433ba52ccfefa556bb8f1bc88eb9bac
Mårten Hansson <hmrten@gmail.com>
2016-06-21 08:12:04 +0200

    fix null ref exception in FaultReportDetailsViewModel

--- IDUNv2/ViewModels/FaultReportDetailsViewModel.cs
+++ IDUNv2/ViewModels/FaultReportDetailsViewModel.cs
@@ -12,32 +12,50 @@ namespace IDUNv2.ViewModels
 {
     class FaultReportDetailsViewModel : NotifyBase
     {
+        #region Fields
 
         private IFaultReportAccess faultReportAccess;
 
+        #endregion
+
+        #region Notify Fields
+
         private FaultReport model;
+
+        #endregion
+
+        #region Notify Properties
+
+        public FaultReport Model
+        {
+            get { return model; }
+            set { model = value; Notify(); }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Discovery
+        {
+            get { return faultReportAccess.LookupWorkOrderDiscovery(model.ErrDiscoverCode)?.Description; }
+        }
+
+        public string Symptom
+        {
+            get { return faultReportAccess.LookupWorkOrderSymptom(model.ErrSymptom)?.Description; }
+        }
+
+        public string Priority
+        {
+            get { return faultReportAccess.LookupMaintenancePriority(model.PriorityId)?.Description; }
+        }
+
+        #endregion
 
         public FaultReportDetailsViewModel(IFaultReportAccess faultReportAccess)
         {
             this.faultReportAccess = faultReportAccess;
         }
-
-        public FaultReport Model { get { return model; } set { model = value; Notify(); } }
-
-        public string Discovery
-        {
-            get { return faultReportAccess.LookupWorkOrderDiscovery(model.ErrDiscoverCode).Description; }
-        }
-
-        public string Symptom
-        {
-            get { return faultReportAccess.LookupWorkOrderSymptom(model.ErrSymptom).Description; }
-        }
-
-        public string Priority
-        {
-            get { return faultReportAccess.LookupMaintenancePriority(model.PriorityId).Description; }
-        }
-
     }
 }
