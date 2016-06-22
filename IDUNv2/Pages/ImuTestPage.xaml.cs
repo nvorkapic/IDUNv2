a4b16bf2e4acace3a216748c3139e2529b3ebd76
nvorkapic <nikola.vorkapic@hotmail.com>
2016-06-22 15:59:17 +0200

    IMU ViewModel

--- IDUNv2/Pages/ImuTestPage.xaml.cs
+++ IDUNv2/Pages/ImuTestPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using IDUNv2.DataAccess;
+using IDUNv2.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -18,26 +19,7 @@ namespace IDUNv2.Pages
 {
     public sealed partial class ImuTestPage : Page
     {
-        double gxmax = double.MinValue;
-        double gxmin = double.MaxValue;
-        double gymax = double.MinValue;
-        double gymin = double.MaxValue;
-        double gzmax = double.MinValue;
-        double gzmin = double.MaxValue;
-        double gmax = double.MinValue;
-        double gmin = double.MaxValue;
-
-        double mmax = double.MinValue;
-        double mmin = double.MaxValue;
-
-        double axmax = double.MinValue;
-        double axmin = double.MaxValue;
-        double aymax = double.MinValue;
-        double aymin = double.MaxValue;
-        double azmax = double.MinValue;
-        double azmin = double.MaxValue;
-        double amax = double.MinValue;
-        double amin = double.MaxValue;
+        IMUViewModel viewModel = new IMUViewModel();
 
         private DispatcherTimer timer = new DispatcherTimer
         {
@@ -55,136 +37,77 @@ namespace IDUNv2.Pages
         private void Timer_Tick(object sender, object e)
         {
             var r = DAL.ImuSensorWatcher.Readings;
-            if (r.FusionPoseValid)
-            {
-                
-                PitchAxis.Value = r.FusionPose.AsDegrees.X;
-                PitchAxisValue.Text = r.FusionPose.AsDegrees.X.ToString("0.0000");
-                RollAxis.Value = r.FusionPose.AsDegrees.Y;
-                RollAxisValue.Text = r.FusionPose.AsDegrees.Y.ToString("0.0000");
-                JawAxis.Value = r.FusionPose.AsDegrees.Z;
-                JawAxisValue.Text = r.FusionPose.AsDegrees.Z.ToString("0.0000");
-            }
+            viewModel.GetAcceleration(r);
+            viewModel.GetGyroscope(r);
+            viewModel.GetMagneticField(r);
+            viewModel.GetFusionPose(r);
 
-            if (r.AccelerationValid)
-            {
-                AccelerationX.Value = r.Acceleration.X;
-                if (axmax < r.Acceleration.X)
-                    axmax = r.Acceleration.X;
-                if (axmin > r.Acceleration.X)
-                    axmin = r.Acceleration.X;
+            #region FusionPose
+            PitchAxis.Value = viewModel.PitchAxis;
+            PitchAxisValue.Text = viewModel.PitchAxis.ToString("0.0000");
+            RollAxis.Value = viewModel.RollAxis;
+            RollAxisValue.Text = viewModel.RollAxis.ToString("0.0000");
+            JawAxis.Value = viewModel.JawAxis;
+            JawAxisValue.Text = viewModel.JawAxis.ToString("0.0000");
+            #endregion
 
-                AccelerationX.Minimum = axmin;
-                AccelerationX.Maximum = axmax;
+            #region Acceleration
+            AccelerationX.Value = viewModel.Acceleration.X;
+            AccelerationX.Maximum = viewModel.AccelerationMax.X;
+            AccelerationX.Minimum = viewModel.AccelerationMin.X;
+            AccelerationXValue.Text = viewModel.Acceleration.X.ToString("0.0000");
 
-                AccelerationXValue.Text = r.Acceleration.X.ToString("0.0000");
+            AccelerationY.Value = viewModel.Acceleration.Y;
+            AccelerationY.Minimum = viewModel.AccelerationMin.Y;
+            AccelerationY.Maximum = viewModel.AccelerationMax.Y;
+            AccelerationYValue.Text = viewModel.Acceleration.Y.ToString("0.0000");
 
-                AccelerationY.Value = r.Acceleration.Y;
-                if (aymax < r.Acceleration.Y)
-                    aymax = r.Acceleration.Y;
-                if (aymin > r.Acceleration.Y)
-                    aymin = r.Acceleration.Y;
+            AccelerationZ.Value = viewModel.Acceleration.Z;
+            AccelerationZ.Minimum = viewModel.AccelerationMin.Z;
+            AccelerationZ.Maximum = viewModel.AccelerationMax.Z;
+            AccelerationZValue.Text = viewModel.Acceleration.Z.ToString("0.0000");
+     
+            Accel.Value = viewModel.AcceloScalar;
+            Accel.Minimum = viewModel.amin;
+            Accel.Maximum = viewModel.amax;
+            AccelValue.Text = viewModel.AcceloScalar.ToString("0.0000");
+            #endregion
 
-                AccelerationY.Minimum = aymin;
-                AccelerationY.Maximum = aymax;
+            #region MagneticField
+            MagnetX.Value = viewModel.Magnet.X;
+            MagnetXValue.Text = viewModel.Magnet.X.ToString("0.0000");
+            MagnetY.Value = viewModel.Magnet.Y;
+            MagnetYValue.Text = viewModel.Magnet.Y.ToString("0.0000");
+            MagnetZ.Value = viewModel.Magnet.Z;
+            MagnetZValue.Text = viewModel.Magnet.Z.ToString("0.0000");
 
-                AccelerationYValue.Text = r.Acceleration.Y.ToString("0.0000");
+            Magnet.Value = viewModel.MagnetScalar;
+            Magnet.Minimum = viewModel.mmin;
+            Magnet.Maximum = viewModel.mmax;
+            MagnetValue.Text = viewModel.MagnetScalar.ToString("0.0000");
+            #endregion
 
-                AccelerationZ.Value = r.Acceleration.Z;
-                if (azmax < r.Acceleration.Z)
-                    azmax = r.Acceleration.Z;
-                if (azmin > r.Acceleration.Z)
-                    azmin = r.Acceleration.Z;
+            #region Gyroscope
+            GyroX.Value = viewModel.Gyro.X;
+            GyroX.Minimum = viewModel.GyroMin.X;
+            GyroX.Maximum = viewModel.GyroMax.X;
+            GyroXValue.Text = viewModel.Gyro.X.ToString("0.0000");
 
-                AccelerationZ.Minimum = azmin;
-                AccelerationZ.Maximum = azmax;
+            GyroY.Value = viewModel.Gyro.Y;
+            GyroY.Minimum = viewModel.GyroMin.Y;
+            GyroY.Maximum = viewModel.GyroMax.Y;
+            GyroYValue.Text = viewModel.Gyro.Y.ToString("0.0000");
 
-                AccelerationZValue.Text = r.Acceleration.Z.ToString("0.0000");
+            GyroZ.Value = viewModel.Gyro.Z;
+            GyroZ.Minimum = viewModel.GyroMin.Z;
+            GyroZ.Maximum = viewModel.GyroMax.Z;
+            GyroZValue.Text =viewModel.Gyro.Z.ToString("0.0000");
 
-                var AcceloScalar = Math.Sqrt(Math.Pow(r.Acceleration.X, 2) + Math.Pow(r.Acceleration.Y, 2) + Math.Pow(r.Acceleration.Z, 2));
-                Accel.Value = AcceloScalar;
-                if (amax < AcceloScalar)
-                    amax = AcceloScalar;
-                if (amin > AcceloScalar)
-                    amin = AcceloScalar;
-
-                Accel.Minimum = amin;
-                Accel.Maximum = amax;
-
-                AccelValue.Text = AcceloScalar.ToString("0.0000");
-
-
-            }
-
-            if (r.MagneticFieldValid)
-            {
-                MagnetX.Value = r.MagneticField.X;
-                MagnetXValue.Text = r.MagneticField.X.ToString("0.0000");
-                MagnetY.Value = r.MagneticField.Y;
-                MagnetYValue.Text = r.MagneticField.Y.ToString("0.0000");
-                MagnetZ.Value = r.MagneticField.Z;
-                MagnetZValue.Text = r.MagneticField.Z.ToString("0.0000");
-
-                var MagnetScalar = Math.Sqrt(Math.Pow(r.MagneticField.X, 2) + Math.Pow(r.MagneticField.Y, 2) + Math.Pow(r.MagneticField.Z, 2));
-                Magnet.Value = MagnetScalar;
-                if (mmax < MagnetScalar)
-                    mmax = MagnetScalar;
-                if (mmin > MagnetScalar)
-                    mmin = MagnetScalar;
-
-                MagnetValue.Text = MagnetScalar.ToString("0.0000");
-
-            }
-
-
-            if (r.GyroValid)
-            {
-                GyroX.Value = r.Gyro.X;
-                if (gxmax < r.Gyro.X)
-                    gxmax = r.Gyro.X;
-                if (gxmin > r.Gyro.X)
-                    gxmin = r.Gyro.X;
-
-                GyroX.Minimum = gxmin;
-                GyroX.Maximum = gxmax;
-
-                GyroXValue.Text = r.Gyro.AsDegrees.X.ToString("0.0000");
-
-                GyroY.Value = r.Gyro.Y;
-                if (gymax < r.Gyro.Y)
-                    gymax = r.Gyro.Y;
-                if (gymin > r.Gyro.Y)
-                    gymin = r.Gyro.Y;
-
-                GyroY.Minimum = gymin;
-                GyroY.Maximum = gymax;
-
-                GyroYValue.Text = r.Gyro.AsDegrees.Y.ToString("0.0000");
-
-                GyroZ.Value = r.Gyro.X;
-                if (gzmax < r.Gyro.Z)
-                    gzmax = r.Gyro.Z;
-                if (gzmin > r.Gyro.Z)
-                    gzmin = r.Gyro.Z;
-
-                GyroZ.Minimum = gzmin;
-                GyroZ.Maximum = gzmax;
-
-                GyroZValue.Text = r.Gyro.AsDegrees.Z.ToString("0.0000");
-
-                var GyroScalar = Math.Sqrt(Math.Pow(r.Gyro.AsDegrees.X, 2) + Math.Pow(r.Gyro.AsDegrees.Y, 2) + Math.Pow(r.Gyro.AsDegrees.Z, 2));
-                Gyro.Value = GyroScalar;
-                if (gmax < GyroScalar)
-                    gmax = GyroScalar;
-                if (gmin > GyroScalar)
-                    gmin = GyroScalar;
-
-                Gyro.Minimum = gmin;
-                Gyro.Maximum = gmax;
-
-                GyroValue.Text = GyroScalar.ToString("0.0000");
-            }
-           
+            Gyro.Value = viewModel.GyroScalar;
+            Gyro.Minimum = viewModel.gmin;
+            Gyro.Maximum = viewModel.gmax;
+            GyroValue.Text = viewModel.GyroScalar.ToString("0.0000");
+            #endregion
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
---
+++ IDUNv2/ViewModels/IMUViewModel.cs
@@ -0,0 +1,89 @@
+﻿using IDUNv2.DataAccess;
+using IDUNv2.SensorLib.IMU;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDUNv2.ViewModels
+{
+    public class IMUViewModel
+    {
+
+        public Vector3 Acceleration;
+        public Vector3 AccelerationMax = new Vector3(double.MinValue, double.MinValue, double.MinValue);
+        public Vector3 AccelerationMin = new Vector3(double.MaxValue, double.MaxValue, double.MaxValue);
+        public double AcceloScalar;
+        public double amax = double.MinValue;
+        public double amin = double.MaxValue;
+
+        public Vector3 Magnet;
+        public double MagnetScalar;
+        public double mmax;
+        public double mmin;
+
+        public Vector3 Gyro;
+        public Vector3 GyroMax = new Vector3(double.MinValue, double.MinValue, double.MinValue);
+        public Vector3 GyroMin = new Vector3(double.MaxValue, double.MaxValue, double.MaxValue);
+        public double GyroScalar;
+        public double gmax = double.MinValue;
+        public double gmin = double.MaxValue;
+
+        public double PitchAxis;
+        public double RollAxis;
+        public double JawAxis;
+
+
+        
+        public void GetAcceleration(ImuSensorReadings r)
+        {
+            Acceleration = r.Acceleration;
+            AccelerationMin = GetVectorMin(Acceleration, AccelerationMin);
+            AccelerationMax = GetVectorMax(Acceleration, AccelerationMax);
+            AcceloScalar = Math.Sqrt(Math.Pow(r.Acceleration.X, 2) + Math.Pow(r.Acceleration.Y, 2) + Math.Pow(r.Acceleration.Z, 2));
+            amax = Math.Max(AcceloScalar, amax);
+            amin = Math.Min(AcceloScalar, amin);
+        }
+
+        public void GetMagneticField(ImuSensorReadings r)
+        {
+            Magnet = r.MagneticField;
+
+            MagnetScalar = Math.Sqrt(Math.Pow(r.MagneticField.X, 2) + Math.Pow(r.MagneticField.Y, 2) + Math.Pow(r.MagneticField.Z, 2));
+            mmax = Math.Max(MagnetScalar, mmax);
+            mmin = Math.Min(MagnetScalar, mmin);
+        }
+
+        public void GetGyroscope(ImuSensorReadings r)
+        {
+            Gyro = r.Gyro;
+            GyroMax = GetVectorMax(GyroMax, Gyro);
+            GyroMin = GetVectorMin(GyroMin, Gyro);
+            GyroScalar = Math.Sqrt(Math.Pow(r.Gyro.AsDegrees.X, 2) + Math.Pow(r.Gyro.AsDegrees.Y, 2) + Math.Pow(r.Gyro.AsDegrees.Z, 2));
+            gmax = Math.Max(GyroScalar, gmax);
+            gmin = Math.Min(GyroScalar, gmin);
+        }
+
+        public Vector3 GetVectorMax(Vector3 vectorA, Vector3 vectorB)
+        {
+            Vector3 v = new Vector3(Math.Max(vectorA.X, vectorB.X), Math.Max(vectorA.Y, vectorB.Y), Math.Max(vectorA.Z, vectorB.Z));
+            return v;
+        }
+
+        public Vector3 GetVectorMin(Vector3 vectorA, Vector3 vectorB)
+        {
+            Vector3 v = new Vector3(Math.Min(vectorA.X, vectorB.X), Math.Min(vectorA.Y, vectorB.Y), Math.Min(vectorA.Z, vectorB.Z));
+            return v;
+        }
+        
+
+
+        public void GetFusionPose(ImuSensorReadings r)
+        {
+            PitchAxis = r.FusionPose.AsDegrees.X;
+            RollAxis = r.FusionPose.AsDegrees.Y;
+            JawAxis = r.FusionPose.AsDegrees.Z;
+        }
+    }
+}
