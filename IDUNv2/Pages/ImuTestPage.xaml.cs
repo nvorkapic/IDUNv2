b4ac89509fa506fa42ee7756f4269b08bc8d8d2e
nvorkapic <nikola.vorkapic@hotmail.com>
2016-06-22 09:30:10 +0200

    IMU Sensors

--- IDUNv2/Pages/ImuTestPage.xaml.cs
+++ IDUNv2/Pages/ImuTestPage.xaml.cs
@@ -36,15 +36,44 @@ namespace IDUNv2.Pages
             var r = DAL.ImuSensorWatcher.Readings;
             if (r.FusionPoseValid)
             {
-                Pose.Text = r.FusionPose.ToString(true);
+                PitchAxis.Value = r.FusionPose.AsDegrees.X;
+                PitchAxisValue.Text = r.FusionPose.AsDegrees.X.ToString("0.0000") + " °";
+                RollAxis.Value = r.FusionPose.AsDegrees.Y;
+                RollAxisValue.Text = r.FusionPose.AsDegrees.Y.ToString("0.0000") + " °";
+                JawAxis.Value = r.FusionPose.AsDegrees.Z;
+                JawAxisValue.Text = r.FusionPose.AsDegrees.Z.ToString("0.0000") + " °";
             }
-            MagnetProgressBarX.Value =r.FusionPose.AsDegrees.X;
-            MagnetProgressBarY.Value =r.FusionPose.AsDegrees.Y;
-            MagnetProgressBarZ.Value =r.FusionPose.AsDegrees.Z;
-            
-            MFX.Text = r.FusionPose.AsDegrees.X.ToString();
-            MFY.Text = r.FusionPose.AsDegrees.Y.ToString();
-            MFZ.Text = r.FusionPose.AsDegrees.Z.ToString();
+
+            if (r.AccelerationValid)
+            {
+                AccelerationX.Value = r.Acceleration.X;
+                AccelerationXValue.Text = r.Acceleration.X.ToString("0.0000") + " g";
+                AccelerationY.Value = r.Acceleration.X;
+                AccelerationYValue.Text = r.Acceleration.Y.ToString("0.0000") + " g";
+                AccelerationZ.Value = r.Acceleration.X;
+                AccelerationZValue.Text = r.Acceleration.Z.ToString("0.0000") + " g";
+            }
+
+            if (r.MagneticFieldValid)
+            {
+                MagnetX.Value = r.MagneticField.X;
+                MagnetXValue.Text = r.MagneticField.X.ToString("0.0000") + " uT";
+                MagnetY.Value = r.MagneticField.Y;
+                MagnetYValue.Text = r.MagneticField.Y.ToString("0.0000") + " uT";
+                MagnetZ.Value = r.MagneticField.Z;
+                MagnetZValue.Text = r.MagneticField.Z.ToString("0.0000") + " uT";
+            }
+
+            if (r.GyroValid)
+            {
+                GyroX.Value = r.Gyro.X;
+                GyroXValue.Text = r.Gyro.AsDegrees.X.ToString("0.0000") + " °/s";
+                GyroY.Value = r.Gyro.Y;
+                GyroYValue.Text = r.Gyro.AsDegrees.Y.ToString("0.0000") + " °/s";
+                GyroZ.Value = r.Gyro.X;
+                GyroZValue.Text = r.Gyro.AsDegrees.Z.ToString("0.0000") + " °/s";
+            }
+           
         }
     }
 }
