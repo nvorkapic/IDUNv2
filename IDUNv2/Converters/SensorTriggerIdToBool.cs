589441d06b8a927285068509c3ef6556dd53a361
Mårten Hansson <hmrten@gmail.com>
2016-06-28 13:58:42 +0200

    fix bug when enabled Triggers without saving first
    If you tried enabling a SensorTrigger before it was saved (and had an Id
    of 0) it would add it as an active trigger, but would of course always
    fail to find from the sqlite db.
    This disabled the checkbox until you've saved the trigger.

---
+++ IDUNv2/Converters/SensorTriggerIdToBool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace IDUNv2.Converters
+{
+    public class SensorTriggerIdToBool : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value == null ? false : (int)value != 0;
+        }
+
+        // not used
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return 0;
+        }
+    }
+}
